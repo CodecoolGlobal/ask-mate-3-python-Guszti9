fc6fd1f8cb1f9933eae14d5fab66a091ad03fb2f
benedekmolnar <molnar.benedek@gmail.com>
2021-10-11 13:06:46 +0200

    Appending to files added

--- data_manager.py
+++ data_manager.py
@@ -1,26 +1,44 @@
 import csv
 
 def read_from_file(filename):
-    with open(filename, 'r') as csvfile:
+    with open(filename, 'r', newline='') as csvfile:
         list_of_data = []
         data = csv.reader(csvfile)
         for row in data:
             list_of_data.append(row)
     return list_of_data
+
+
 def read_from_dict_file(filename, fieldnames):
-    with open(filename, 'r') as csvfile:
+    with open(filename, 'r', newline='') as csvfile:
         list_of_data = []
         data = csv.DictReader(csvfile, fieldnames=fieldnames)
         for row in data:
             list_of_data.append(row)
     return list_of_data
+
+
 def write_to_file(filename, data_to_write):
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile)
         writer.writerow(data_to_write)
+
+
 def write_to_dict_file(filename, data_to_write, fieldnames):
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(data_to_write)
-def append_to_file(filename):
-def append_to_dict_file(filename, fieldnames):
+
+
+def append_to_file(filename, data_to_write):
+    with open(filename, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile)
+        writer.writerow(data_to_write)
+
+
+def append_to_dict_file(filename, data_to_write, fieldnames):
+    with open(filename, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(data_to_write)
+
+        
