2d8129180630914a89e3f081f8cd0cffe005c854
benedekmolnar <molnar.benedek@gmail.com>
2021-10-11 13:03:30 +0200

    Writing to files added

--- data_manager.py
+++ data_manager.py
@@ -14,7 +14,13 @@ def read_from_dict_file(filename, fieldnames):
         for row in data:
             list_of_data.append(row)
     return list_of_data
-def write_to_file(filename):
-def write_to_dict_file(filename, fieldnames):
+def write_to_file(filename, data_to_write):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile)
+        writer.writerow(data_to_write)
+def write_to_dict_file(filename, data_to_write, fieldnames):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(data_to_write)
 def append_to_file(filename):
 def append_to_dict_file(filename, fieldnames):
