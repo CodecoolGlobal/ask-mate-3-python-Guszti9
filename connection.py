13fac32905d6d4159d32daa20915c876c52772c4
Nitram137 <nemethmartin99@gmail.com>
2021-10-11 13:55:20 +0200

    Connection headers, question page template

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import csv
 
-
-HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def read_from_file(filename):
@@ -30,7 +30,7 @@ def write_to_file(filename, data_to_write):
 
 def write_to_dict_file(filename, data_to_write, fieldnames):
     with open(filename, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=HEADER)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow(data_to_write)
 
@@ -43,6 +43,5 @@ def append_to_file(filename, data_to_write):
 
 def append_to_dict_file(filename, data_to_write, fieldnames):
     with open(filename, 'a', newline='') as csvfile:
-        appender = csv.DictWriter(csvfile, fieldnames=HEADER)
+        appender = csv.DictWriter(csvfile, fieldnames=fieldnames)
         appender.writerow(data_to_write)
-
--- server.py
+++ server.py
@@ -10,10 +10,17 @@ def hello():
 
 
 @app.route("/list")
-def list():
+def list_questions():
     list_of_data = connection.read_from_dict_file("sample_data/question.csv")
     print(list_of_data)
     return render_template("list.html", data=list_of_data)
+
+
+@app.route("question/<question_id>")
+def display_question(question_id):
+    question_data = {}
+    answers = []
+    return render_template("question_page.html", question_data=question_data, answers=answers)
 
 
 if __name__ == "__main__":
