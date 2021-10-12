456b2218df62c772273b105a92b541ebee12b506
trbogi <trencsenyi.bogi@gmail.com>
2021-10-12 10:31:24 +0200

    Added filter_answer_by_answe_id function to data manager.

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import connection
 
+
 def filter_answers_by_question_id(question_id):
     all_answers = connection.read_from_dict_file('sample_data/answer.csv')
     filtered = [answer for answer in all_answers if answer['question_id'] == question_id]
@@ -9,3 +10,10 @@ def filter_answers_by_question_id(question_id):
 def filter_question_by_question_id(question_id):
     return connection.read_from_dict_file('sample_data/question.csv')[int(question_id)-1]
 
+
+def filter_answer_by_answer_id(answer_id):
+    all_answers = connection.read_from_dict_file('sample_data/answer.csv')
+    for answer in all_answers:
+        if answer['id'] == answer_id:
+            filtered_answer = answer
+    return filtered_answer
