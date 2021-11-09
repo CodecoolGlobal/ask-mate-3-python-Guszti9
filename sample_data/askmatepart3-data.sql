a68fe67d8561b1aad2f078e0cb3d75e074232241
benedekmolnar <molnar.benedek@gmail.com>
2021-11-09 09:56:44 +0100

    Make login function work properly

--- sample_data/askmatepart3-data.sql
+++ sample_data/askmatepart3-data.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS public.users;
 CREATE TABLE users (
     id serial NOT NULL,
     username text,
-    password CHAR(128),
+    password CHAR(60),
     reputation integer,
     registration_date timestamp without time zone
 );
